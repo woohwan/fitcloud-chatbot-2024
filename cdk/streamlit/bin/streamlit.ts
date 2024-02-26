e53ae4162be2c8e7a84343a8bda2ab0b064a223b
woohwan <whaprk70@naver.com>
2024-02-26 15:52:20 +0900

    https timeout error 발생

--- cdk/streamlit/bin/streamlit.ts
+++ cdk/streamlit/bin/streamlit.ts
@@ -4,6 +4,4 @@ import * as cdk from 'aws-cdk-lib';
 import { StreamlitStack } from '../lib/streamlit-stack';
 
 const app = new cdk.App();
-new StreamlitStack(app, 'StreamlitStack', {
-  
-});
+new StreamlitStack(app, 'StreamlitStack', {});
